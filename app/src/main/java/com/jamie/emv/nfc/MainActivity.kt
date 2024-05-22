fed77113bb923fbb0b2a8edd37ba72ee77d1f17b
thongpham <thongpham95@gmail.com>
2024-05-22 22:16:18 +0700

    implement emv nfc card reader

--- app/src/main/java/com/jamie/emv/nfc/MainActivity.kt
+++ app/src/main/java/com/jamie/emv/nfc/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.jamie.emv.nfc
 
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.IsoDep
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,38 +16,112 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.github.devnied.emvnfccard.parser.EmvTemplate
+import com.jamie.emv.nfc.emvnfc.PcscProvider
 import com.jamie.emv.nfc.ui.theme.EMVNFCTheme
+import java.io.IOException
+import java.time.LocalDate
+import java.time.ZoneId
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), NfcAdapter.ReaderCallback {
+
+    private var mNfcAdapter: NfcAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
         enableEdgeToEdge()
         setContent {
             EMVNFCTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        if (mNfcAdapter != null) {
+            val options = Bundle()
+            options.putInt(NfcAdapter.EXTRA_READER_PRESENCE_CHECK_DELAY, 250)
+
+            mNfcAdapter!!.enableReaderMode(
+                this,
+                this,
+                NfcAdapter.FLAG_READER_NFC_A or
+                        NfcAdapter.FLAG_READER_NFC_B or
+                        NfcAdapter.FLAG_READER_NFC_F or
+                        NfcAdapter.FLAG_READER_NFC_V or
+                        NfcAdapter.FLAG_READER_NFC_BARCODE or
+                        NfcAdapter.FLAG_READER_NO_PLATFORM_SOUNDS,
+                options
+            )
+        }
+    }
+
+    override fun onTagDiscovered(tag: Tag?) {
+        val isoDep: IsoDep?
+        try {
+            isoDep = IsoDep.get(tag)
+            if (isoDep != null) {
+                (getSystemService(VIBRATOR_SERVICE) as Vibrator).vibrate(
+                    VibrationEffect.createOneShot(
+                        150,
+                        10
+                    )
+                )
+            }
+            isoDep.connect()
+            val provider = PcscProvider()
+            provider.setmTagCom(isoDep)
+            val config = EmvTemplate.Config()
+                .setContactLess(true)
+                .setReadAllAids(true)
+                .setReadTransactions(true)
+                .setRemoveDefaultParsers(false)
+                .setReadAt(true)
+            val parser = EmvTemplate.Builder()
+                .setProvider(provider)
+                .setConfig(config)
+                .build()
+            val card = parser.readEmvCard()
+            val cardNumber = card.cardNumber
+            val cardHolder = card.holderFirstname + " " + card.holderLastname
+            val cardType = card.type.getName()
+            Log.d(
+                "PaymentResultCardNumber: ",
+                "Card Number: $cardNumber\nCard Holder: $cardHolder\nType: $cardType"
+            )
+
+            val expireDate = card.expireDate
+            var date = LocalDate.of(1999, 12, 31)
+            if (expireDate != null) {
+                date = expireDate.toInstant()
+                    .atZone(ZoneId.systemDefault())
+                    .toLocalDate()
+            }
+            Log.d("PaymentResultDate: ", date.toString())
+            try {
+                isoDep.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Tap your card!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EMVNFCTheme {
-        Greeting("Android")
-    }
 }
---
+++ app/src/main/java/com/jamie/emv/nfc/emvnfc/PcscProvider.kt
@@ -0,0 +1,38 @@
+package com.jamie.emv.nfc.emvnfc
+
+import android.nfc.tech.IsoDep
+import com.github.devnied.emvnfccard.exception.CommunicationException
+import com.github.devnied.emvnfccard.parser.IProvider
+import java.io.IOException
+
+class PcscProvider : IProvider {
+
+    private lateinit var mTagCom: IsoDep
+
+    @Throws(CommunicationException::class)
+    override fun transceive(pCommand: ByteArray?): ByteArray? {
+        var response: ByteArray? = null
+        try {
+            // send command to emv card
+            if (mTagCom.isConnected) {
+                response = mTagCom.transceive(pCommand)
+            }
+        } catch (e: IOException) {
+            throw CommunicationException(e.message)
+        }
+        return response
+    }
+
+    override fun getAt(): ByteArray {
+        var result: ByteArray?
+        result = mTagCom.historicalBytes // for tags using NFC-B
+        if (result == null) {
+            result = mTagCom.hiLayerResponse // for tags using NFC-B
+        }
+        return result
+    }
+
+    fun setmTagCom(mTagCom: IsoDep) {
+        this.mTagCom = mTagCom
+    }
+}
